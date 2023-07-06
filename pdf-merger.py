2e0e07cd8fe2f6e6387ccc2d1bb75bf519ac3f0f
Jeffery Pan <jdpan002@gmail.com>
2023-07-06 15:58:54 +1000

    added move up and down buttons

--- pdf-merger.py
+++ pdf-merger.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from PyPDF2 import PdfMerger
 
+
 def merge_pdf_files(output_path, files):
     merger = PdfMerger()
 
@@ -10,12 +11,14 @@ def merge_pdf_files(output_path, files):
     merger.write(output_path)
     merger.close()
 
+
 def main():
     layout = [
         [sg.Text('Select PDF files to merge:', font='Any 12')],
-        [sg.InputText('', key='-FILE-', enable_events=True, readonly=True), sg.FilesBrowse()],
+        [sg.InputText('', key='-FILE-', enable_events=True, readonly=True), sg.FilesBrowse(file_types=(('PDF', '*.pdf')))],
         [sg.Listbox([], size=(50, 6), key='-FILE LIST-')],
-        [sg.Button('Merge', key='-MERGE-'), sg.Button('Clear', key='-CLEAR-'), sg.Exit()]
+        [sg.Button('Merge', key='-MERGE-'), sg.Button('Move Up', key='-MOVE UP-'),
+         sg.Button('Move Down', key='-MOVE DOWN-'), sg.Button('Clear', key='-CLEAR-'), sg.Exit()]
     ]
 
     window = sg.Window('PDF Merger', layout)
@@ -36,7 +39,7 @@ def main():
             window['-FILE LIST-'].update(files_to_merge)
 
         if event == '-MERGE-':
-            output_file = sg.popup_get_file('Save merged PDF as:', save_as=True, file_types=(('PDF Files', '*.pdf'),))
+            output_file = sg.popup_get_file('Save merged PDF as:', save_as=True, file_types=(('PDF', '*.pdf'),))
             if output_file:
                 output_file += '.pdf'
                 merge_pdf_files(output_file, files_to_merge)
@@ -48,7 +51,24 @@ def main():
             files_to_merge.clear()
             window['-FILE LIST-'].update(files_to_merge)
 
+        if event == '-MOVE UP-':
+            selected_indices = values['-FILE LIST-']
+            if len(selected_indices) == 1 and selected_indices[0]:
+                index = files_to_merge.index(selected_indices[0])
+                if index != 0:
+                    files_to_merge[index], files_to_merge[index - 1] = files_to_merge[index - 1], files_to_merge[index]
+                    window['-FILE LIST-'].update(files_to_merge)
+
+        if event == '-MOVE DOWN-':
+            selected_indices = values['-FILE LIST-']
+            if len(selected_indices) == 1 and selected_indices[0]:
+                index = files_to_merge.index(selected_indices[0])
+                if index != len(files_to_merge) - 1:
+                    files_to_merge[index], files_to_merge[index + 1] = files_to_merge[index + 1], files_to_merge[index]
+                    window['-FILE LIST-'].update(files_to_merge)
+
     window.close()
+
 
 if __name__ == '__main__':
     main()
